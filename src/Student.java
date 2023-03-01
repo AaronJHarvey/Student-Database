ef852bababedb450806eebb5432d2a0cf9026270
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-03-01 10:35:25 -0500

    Basic Layout

---
+++ src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentId;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfBalance; 
+
+    // Constructor prompt user to enter Student's name and year 
+
+    // Generate ID beginning with their year
+
+    // Enroll in courses
+
+    // View Balance 
+
+    // Pay tuition
+
+    // Show Status (name, Id, courses, balance)
+
+
+}
---
+++ src/StudentDatabaseApp.java
@@ -0,0 +1,15 @@
+public class StudentDatabaseApp {
+     // Ask how many students should be created 
+
+    //Create a new Student * n number of new students - Ask the students Name and Grade/Year
+    
+    // Return the Student's ID beginning with their grade
+
+    // Ask the student to pick the course(s) they wish to enroll in add $600 to their balance for each
+    
+    // Getter for the Student's Balance 
+
+    // Setter for the students balance when they make a payment 
+
+    // Getter for the Student Status - Returning their name, ID, courses, and balance 
+}
